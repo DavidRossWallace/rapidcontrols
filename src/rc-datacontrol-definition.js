65fb17c21d309fb573654f48b5c6e0909c117257
DavidRossWallace <36711654+DavidRossWallace@users.noreply.github.com>
2020-05-30 07:10:49 -0700

    Create rc-datacontrol-definition.js

---
+++ src/rc-datacontrol-definition.js
@@ -0,0 +1,77 @@
+if (typeof rapidControls === 'undefined') {
+	var rapidControls={};
+}
+if (!rapidControls.hasOwnProperty("dataControl")) {
+	rapidControls.dataControl={};
+}
+rapidControls.dataControl.definition=(function() {
+			var controlDefinitions={};
+			var getDefinition=function($control,callback,reload) {
+					var $c=$control;
+					var definitionSrc="file";
+					var newDefinition={};
+					var controlId=$c.attr("id");
+					console.log("definition being retrieved for:" + controlId)
+				
+					if ($c[0].hasAttribute("data-definition-src")) {
+						definitionSrc=$c.attr("data-definition-src");
+					}						
+					if (definitionSrc=="proxy") {
+						console.log("about to return proxy def:" + JSON.stringify(controlDefinitions[$c.attr("data-definition-proxy")]));
+						callback(controlDefinitions[$c.attr("data-definition-proxy")]);						
+					} else {
+						if ((controlDefinitions.hasOwnProperty(controlId)) && (reload!=true)) {
+							callback(controlDefinitions[controlId]);
+						} else {
+							var defPath=($c[0].hasAttribute("data-definition-url")) ? $c.attr("data-definition-url") : "";
+							if (defPath=="") {
+								if (typeof callback==="function") {
+									callback({});
+								} else {
+									return {}
+								}
+							} else {
+							getJSON({"file":defPath},function(pDefinition) {
+								console.log("retrieving defition from url:" + defPath);
+								console.log("retrieved definition:" + JSON.stringify(pDefinition));
+								controlDefinitions[controlId]=pDefinition;
+								if (typeof callback==="function") {
+									callback(pDefinition);
+								} else {
+									return pDefinition
+								}
+							});
+							}
+						}
+					}
+				
+			}
+			
+			var setDefinition=function(defid,def) {
+				controlDefinitions[defid]=def;				
+			}
+
+				var getJSON=function(package, callback) {
+					var mydata;
+
+					var pDataType = (package.dataType == "js") ? "JSON" : "JSON";
+
+					$.ajax({
+						dataType: pDataType,
+						url: package.file,
+						async: true,
+						data: {
+							package: JSON.stringify(package)
+						},
+						type: 'POST',
+						success: function (data) {
+							callback(data);
+						}
+					})
+				}
+				return {
+					getDefinition:getDefinition,
+					setDefinition:setDefinition
+				}
+			
+			})();
